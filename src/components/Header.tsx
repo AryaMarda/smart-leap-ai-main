37100b33df66af3189fab09d47fb96aeca0b89ce
Arya Marda <aryamarda@google.com>
2025-09-06 10:00:47 +0000

    Added comapany name

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="absolute top-0 left-0 right-0 z-10 py-6">
+      <div className="container mx-auto px-4 lg:px-8 flex justify-between items-center">
+        <h1 className="text-3xl font-bold text-foreground tracking-tight text-primary">
+          HelloAI
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/HeroSection";
 import ProblemSection from "@/components/ProblemSection";
 import SolutionSection from "@/components/SolutionSection";
 import ConsultationForm from "@/components/ConsultationForm";
+import Header from "@/components/Header";
 
 const Index = () => {
   const scrollToForm = () => {
@@ -13,6 +14,7 @@ const Index = () => {
 
   return (
     <div className="min-h-screen">
+      <Header />
       <HeroSection onBookConsultation={scrollToForm} />
       <ProblemSection />
       <SolutionSection onBookConsultation={scrollToForm} />
